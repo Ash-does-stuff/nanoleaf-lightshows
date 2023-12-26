42165e0ec8f4cda8d0a95a59c0b43c995a763cd5
Ashley <ashhh.does.stuff@gmail.com>
2023-12-26 17:03:49 +0100

    fileTranslator fix

--- fileTranslator.py
+++ fileTranslator.py
@@ -4,31 +4,30 @@ fileName = "handCrushed"
 timeOffset = 0
 
 dictionary = {
-    "a": "34881",
-    "b": "64611",
-    "c": "40291",
-    "d": "24026",
-    "e": "3467",
-    "f": "8174",
-    "g": "35662",
-    "h": "19806",
-    "i": "58321",
-    "j": "38247",
-    "k": "16905",
-    "l": "16322",
-    "m": "63080",
-    "n": "50002",
-    "o": "40125"
+    "a": 34881,
+    "b": 64611,
+    "c": 40291,
+    "d": 24026,
+    "e": 3467,
+    "f": 8174,
+    "g": 35662,
+    "h": 19806,
+    "i": 58321,
+    "j": 38274,
+    "k": 16905,
+    "l": 16322,
+    "m": 63080,
+    "n": 50002,
+    "o": 40125
 }
 
 file = open(fileName + ".json", "r")
-text = json.load(file.read())
+fileJson = json.loads(file.read())
 
-for action in text["actions"]:
-    action["time"] = action["time"] + timeOffset
-    action["panel_id"] = dictionary[action["panel_id"]]
-
-print(text)
+for action in fileJson["actions"]:
+    action["time"] = float(action["time"]) + timeOffset
+    if action["action"] != "set":
+        action["panel_id"] = dictionary[action["panel_id"]]
 
 f = open(fileName + "-converted.json", "w")
-f.write(text)
+f.write(json.dumps(fileJson))
