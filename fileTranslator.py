ebb378b0f6a90ef77b7c3e0e8e1dd9d09bfc5947
Ashley <ashhh.does.stuff@gmail.com>
2023-12-30 20:11:56 +0100

    updated fileTranslator.py
    now you have to include the dictionary in the lightshow file itself
    
    how convenient

--- fileTranslator.py
+++ fileTranslator.py
@@ -1,36 +1,20 @@
 import json
 
-fileName = "handCrushed-final"
-timeOffset = 30
+fileName = "test"
+timeOffset = 0
 
-changeTime = True
-changeIDs = False
-
-dictionary = {
-    "a": 34881,
-    "b": 64611,
-    "c": 40291,
-    "d": 24026,
-    "e": 3467,
-    "f": 8174,
-    "g": 35662,
-    "h": 19806,
-    "i": 58321,
-    "j": 38274,
-    "k": 16905,
-    "l": 16322,
-    "m": 63080,
-    "n": 50002,
-    "o": 40125
-}
+changeTime = False
+changeIDs = True
 
 file = open(fileName + ".json", "r")
 fileJson = json.loads(file.read())
 
+dictionary = fileJson["dictionary"]
+
 for action in fileJson["actions"]:
     if changeTime:
         action["time"] = float(action["time"]) + timeOffset
-    if changeIDs and action["action"] != "set":
+    if changeIDs and action["action"] != "set" and action["panel_id"] != "RAND":
         action["panel_id"] = dictionary[action["panel_id"]]
 
 fileJson["actions"].sort(key=(lambda action : action["time"]))
